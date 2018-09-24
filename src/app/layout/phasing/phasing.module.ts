cd8f3233a089055cd920d08391bf7b3890dec5a5
Tornz <cantornaeric2024@gmail.com>
2018-09-24 18:01:44 +0800

    update fpa modules

--- src/app/layout/phasing/phasing.module.ts
+++ src/app/layout/phasing/phasing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+
 import { Select2Module } from 'ng2-select2';
 
 import { PhasingRoutingModule } from './phasing-routing.module';
