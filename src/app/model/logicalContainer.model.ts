c94f8dc645d56977e3ca85f18925e3ea4b0b26f5
Richard Joseph M. Santiago <richardjosephm.santiago@pccw.com>
2018-09-27 19:45:11 +0800

    09272018 Update Logical Container for delete.

--- src/app/model/logicalContainer.model.ts
+++ src/app/model/logicalContainer.model.ts
@@ -5,8 +5,23 @@ export class LogicalContainer{
     dateUpdated: Date;
     createdBy: string;
     updatedBy: string;
+    type: string;
     appServer: string;
     dbServer: string;
     msServer: string;
-    type: string;
+
+    constructor(id: number, description: string, dateCreated: Date, dateUpdated: Date, createdBy: string, 
+        updatedBy: string, type: string, appServer: string, dbServer: string, msServer: string) {
+    
+    this.id = id;
+    this.description = description;
+    this.dateCreated = dateCreated;
+    this.dateUpdated = dateUpdated;
+    this.createdBy = createdBy;
+    this.updatedBy = updatedBy;
+    this.type = type;
+    this.appServer = appServer;
+    this.dbServer = dbServer;
+    this.msServer = msServer;
+    }
 }
