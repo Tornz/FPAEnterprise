2bcfc73127348b097f5660948f8128c68720c935
Dom <jandominique.logmao@pccw.com>
2018-09-28 14:11:18 +0800

    Project page changes

--- src/app/model/project.model.ts
+++ src/app/model/project.model.ts
@@ -2,9 +2,10 @@ export class Project{
     id: number;
     projectName: string;
     description: Text;
-    phaseNumber = [];
-    technology: string;
-    methodology: string;
+    phases = [];
+    duration: number;
+    // technology: string;
+    // methodology: string;
     dateCreated: Date;
     dateUpdated: Date;
 }
