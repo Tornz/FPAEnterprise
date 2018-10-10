24f6a0abf3db66df3edcd7f964c5779994f81e7b
luaplee <paul.earnel.lee@gmail.com>
2018-10-10 16:39:49 +0800

    Add spinner on lazy loading of child module

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterEvent } from '@angular/router';
 
 @Component({
     selector: 'app-layout',
@@ -6,14 +7,34 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
-    
+    loading: boolean;
     collapedSideBar: boolean;
 
-    constructor() {}
+    constructor(private router: Router) {
+        this.loading = true;
+        this.router.events.subscribe((event: RouterEvent) => {
+            this.routerEventInterceptor(event)
+        })
+    }
 
-    ngOnInit() {}
+    ngOnInit() { }
 
     receiveCollapsed($event) {
         this.collapedSideBar = $event;
+    }
+
+    routerEventInterceptor(event: RouterEvent) {
+        if (event instanceof NavigationStart) {
+            this.loading = true
+        }
+        if (event instanceof NavigationEnd) {
+            this.loading = false
+        }
+        if (event instanceof NavigationCancel) {
+            this.loading = false
+        }
+        if (event instanceof NavigationError) {
+            this.loading = false
+        }
     }
 }
