4498a5c46c97c5c4b00a640f728f447c45e91765
luaplee <paul.earnel.lee@gmail.com>
2018-10-10 17:18:57 +0800

    Remove logical container

--- src/app/layout/phasing/phasing.module.ts
+++ src/app/layout/phasing/phasing.module.ts
@@ -1,36 +1,32 @@
-import { HardwareComponent } from './fpa/hardware/hardware.component';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HotTableModule } from '@handsontable/angular';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Select2Module } from 'ng2-select2';
-
+import { BacklogServices } from '../../data-services/backlog.services';
+import { ComponentServices } from '../../data-services/component.services';
+import { FunctionsServices } from '../../data-services/functions.services';
+import { HardwareService } from '../../data-services/hardware.services';
+import { MappingServices } from '../../data-services/mapping.services';
+import { ModulesServices } from '../../data-services/modules.services';
+import { TechComponentServices } from '../../data-services/techComponent.services';
+import { UserStoryServices } from '../../data-services/userStory.services';
+import { TableFilterPipe } from '../../shared/pipes/table-filter.pipe';
+import { BackLogsComponent } from './fpa/backlogs/backlogs.component';
+import { ComponentsComponent } from './fpa/components/components.component';
+import { EstimationComponent } from './fpa/estimation/estimation.component';
+import { FPAComponent } from './fpa/fpa.component';
+import { FunctionsComponent } from './fpa/functions/functions.component';
+import { HardwareComponent } from './fpa/hardware/hardware.component';
+import { ImplementationComponent } from './fpa/implementation/implementation.component';
+import { MappingComponent } from './fpa/mapping/mapping.component';
+import { ModulesComponent } from './fpa/modules/modules.component';
+import { PhysicalContainerComponent } from './fpa/physical-container/physical-container.component';
+import { SoftwareCategoryComponent } from './fpa/software-component/software-component.component';
+import { UserStoryComponent } from './fpa/user-story/user-story.component';
 import { PhasingRoutingModule } from './phasing-routing.module';
 import { PhasingComponent } from './phasing.component';
-import { FPAComponent } from './fpa/fpa.component';
-import { BackLogsComponent } from './fpa/backlogs/backlogs.component';
-import { EstimationComponent } from './fpa/estimation/estimation.component';
-import { ImplementationComponent } from './fpa/implementation/implementation.component';
-import { HotTableModule } from '@handsontable/angular';
-import { LogicalContainerComponent } from './fpa/logicalContainer/logicalContainer.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ComponentsComponent } from './fpa/components/components.component';
-import { UserStoryComponent } from './fpa/user-story/user-story.component';
-import { UserStoryServices } from '../../data-services/userStory.services'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BacklogServices } from '../../data-services/backlog.services';
-import { TechComponentServices } from '../../data-services/techComponent.services';
-import { LogicalContainerServices } from '../../data-services/logicalContainer.services';
-import { FunctionsServices } from '../../data-services/functions.services';
-import { PhysicalContainerComponent } from './fpa/physical-container/physical-container.component';
-import { TableFilterPipe } from '../../shared/pipes/table-filter.pipe';
-import { ComponentServices } from '../../data-services/component.services';
-import { MappingComponent } from './fpa/mapping/mapping.component'
-import { MappingServices} from '../../data-services/mapping.services';
-import { FunctionsComponent } from './fpa/functions/functions.component';
-import { SoftwareCategoryComponent } from './fpa/software-component/software-component.component';
-import { ModulesComponent } from './fpa/modules/modules.component'
-import { ModulesServices } from '../../data-services/modules.services';
-import { HardwareService } from '../../data-services/hardware.services';
 
 @NgModule({
         imports: [
@@ -51,7 +47,6 @@ import { HardwareService } from '../../data-services/hardware.services';
                 ImplementationComponent,
                 ComponentsComponent,
                 UserStoryComponent,
-                LogicalContainerComponent,
                 PhysicalContainerComponent,
                 TableFilterPipe,
                 FunctionsComponent,
@@ -63,7 +58,6 @@ import { HardwareService } from '../../data-services/hardware.services';
                 BacklogServices,
                 UserStoryServices,
                 TechComponentServices,
-                LogicalContainerServices,
                 ComponentServices,
                 FunctionsServices,
                 MappingServices,
